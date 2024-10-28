b96a9523116a476ebf3860b8830e8ace4a2404fe
Jason Martoux <apps+git@martoux.com>
2024-10-28 18:55:16 +0100

    fix hardhat network thirdweb

--- hardhat/hardhat.config.js
+++ hardhat/hardhat.config.js
@@ -5,7 +5,11 @@ module.exports = {
   solidity: "0.8.27",
   defaultNetwork: "localhost",
   networks: {
+    hardhat: {
+      chainId: 1337,
+    },
     localhost: {
+      chainId: 1337,
       url: "http://127.0.0.1:8545",
     },
   },
